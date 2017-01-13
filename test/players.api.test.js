95a96eab9194ea7ceed951ec5c3125c8818787dd
DrewStock <andrewdstock@gmail.com>
2017-01-12 21:08:54 -0800

    added e2e tests for GET all, GET by id and POST

--- test/players.api.test.js
+++ test/players.api.test.js
@@ -1,13 +1,13 @@
 const chai = require('chai');
-const chaiHttp = require('chai-http');
+// const chaiHttp = require('chai-http');
 const assert = chai.assert;
-chai.use(chaiHttp);
+// chai.use(chaiHttp);
 
 const connection = require( '../lib/setup_mongoose');
 
-const app = require( '../lib/server' );
+const Server = require( '../lib/server' );
 
-describe( 'player', () => {
+describe( 'player e2e', () => {
 
     before( done => {
         const CONNECTED = 1;
@@ -25,50 +25,96 @@ describe( 'player', () => {
         }
     });
 
-    const request = chai.request(app);
+
+
+    it ('GET all', done => {
+
+        const req = {
+            method: 'GET',
+            url: '/api/players'
+        };
+
+        Server.inject (req, res => {
+            console.log(res.payload);
+            assert.deepEqual(res.statusCode, 200);
+            assert.deepEqual(res.payload, '[]');
+            done();
+        });
+
+    });
 
     const antonio = {
-        playerName: 'Antonio Brown'
+        playerName: 'Antonio Brown',
+        position: 'WR'
     };
 
-    it( '/GET all', done => {
-        request
-				.get( '/players' )
-				.then( res => {
-    assert.deepEqual( res.body, [] );
-    done();
-})
-			.catch( done );
-    });
+    it ('/POST', done => {
 
-    it( '/POST', done => {
-        request
-				.post( '/players' )
-				.send( antonio )
-				.then( res => {
-    const player = res.body;
-    // console.log(team);
-    assert.ok( player._id );
-    antonio.__v = 0;
-    antonio._id = player._id;
-    done();
-})
-			.catch( done );
+        const req = {
+            method: 'POST',
+            url: '/api/players',
+            payload: antonio
+        };
+
+        Server.inject (req, res => {
+            const player = res.result;
+            console.log(res.result);
+            antonio._id = player._id;
+            assert.deepEqual(res.statusCode, 200);
+            assert.deepEqual(player.playerName, 'Antonio Brown');
+            // assert.ok(player._id);
+            done();
+        });
 
     });
 
-    it( '/GET by id', done => {
-        request
-    .get( `/players/${antonio._id}` )
-			.then( res => {
-    const player = res.body;
-    assert.deepEqual( player, antonio );
-    done();
-})
-			.catch( done );
+    it ('GET by id', done => {
+
+        const req = {
+            method: 'GET',
+            url: `/api/players/${antonio._id}`
+        };
+
+        Server.inject (req, res => {
+            const player = res.result;
+            console.log(player);
+            assert.deepEqual(res.statusCode, 200);
+            console.log(player._id);
+            console.log(antonio._id);
+            assert.deepEqual(player._id, antonio._id);
+            done();
+        });
+
     });
 
+//     it( '/POST', done => {
+//         request
+// 				.post( '/players' )
+// 				.send( antonio )
+// 				.then( res => {
+//     const player = res.body;
+//     // console.log(team);
+//     assert.ok( player._id );
+//     antonio.__v = 0;
+//     antonio._id = player._id;
+//     done();
+// })
+// 			.catch( done );
+//
+//     });
 
+//     it( '/GET by id', done => {
+//         request
+//     .get( `/players/${antonio._id}` )
+// 			.then( res => {
+//     const player = res.body;
+//     assert.deepEqual( player, antonio );
+//     done();
+// })
+// 			.catch( done );
+//     });
+//
+//
     // after( done => {
     //     connection.close( done );
     // });
